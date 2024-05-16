78dc92bdeae47d3d58b604dc35c6ca5147b93972
Czerwiec <czerrwiec@gmail.com>
2024-05-16 07:33:26 +0200

    Added function for take date and version of file

--- os_try_01.py
+++ os_try_01.py
@@ -1,52 +1,85 @@
 import os
 from os import walk
 from win32api import *
-import numpy as np
+import time
 
-# arr = os.listdir("Z:\\Testy\\2024-05-14 4.0.7 hotfix")
 
-# for i in arr:
-#     print(i)
-#     if i == "MainFiles":
-#         u = os.listdir("Z:\\Testy\\2024-05-14 4.0.7 hotfix\\MainFiles\\V4_I10x64")
-#         print(u)
-#         print()
-#         print()
-# # print(arr)
+
+
+def get_version_number(file_path): 
+  
+    File_information = GetFileVersionInfo(file_path, "\\") 
+  
+    ms_file_version = File_information['FileVersionMS'] 
+    ls_file_version = File_information['FileVersionLS'] 
+
+    list_of_versions = [str(HIWORD(ms_file_version)), str(LOWORD(ms_file_version)), 
+            str(HIWORD(ls_file_version)), str(LOWORD(ls_file_version))]
+    
+    return ".".join(list_of_versions)
+
+    # return [str(HIWORD(ms_file_version)), str(LOWORD(ms_file_version)), 
+    #         str(HIWORD(ls_file_version)), str(LOWORD(ls_file_version))] 
+                
+
+def get_creation_date(path):
+    time_created = time.ctime(os.path.getmtime(path))
+    t_obj = time.strptime(time_created)
+    return  time.strftime("%d-%m-%Y", t_obj)
+
+
+
+
+# file_path = r'Z:\Testy\2024-05-14 4.0.7 hotfix\MainFiles\V4_I10x64\DoorsWindows.dll'
+  
+# version = ".".join(get_version_number(file_path)) 
+  
+# print(version)
+
+
+
 
 dictionary_01 = {}
+
+list_of_data = []
 
 dir_list = []
 name_list = []
 
 
-for (dirpath, dirnames, filenames) in walk("Z:\\Testy\\2024-05-14 4.0.7 hotfix"):
-    print(f"{dirpath}{filenames}")
-   
 
 
-    # print(filenames)
-    # dir_list.append(dirpath)
 
 
-    # name_list.extend(filenames)
+for (dirpath, dirnames, filenames) in walk("C:\\Users\\Czerwiec\\Desktop\\test_folder"):
+
+    for f in filenames:
+
+        path = (os.path.abspath(os.path.join(dirpath, f)))
+        
+        print(path)
+        
+        print(f) 
+        print(get_version_number(path))
+        print(get_creation_date(path))
+
+        x = f + " " + get_version_number(path) + " " + get_creation_date(path)
+
+        list_of_data.append(x)
+     
+       
+
+        dir_list.append(path)
+
+print(list_of_data)
+
+
+# for i in dir_list:
+#     print(".".join(get_version_number(i)))
     
-# print(dir_list[0])
-# print(name_list[0])
-# print()
-# print(dir_list[1])
-# print(name_list[1])
-# print()
-# print(dir_list[2])
-# print(name_list[2])
-# print()
-# print(dir_list[3])
-# print(name_list[3])
 
-    # dir_list.extend(dirpath)
 
-# print(name_list)
-# print(dir_list)
+
 
 
 
@@ -57,21 +90,7 @@ for (dirpath, dirnames, filenames) in walk("Z:\\Testy\\2024-05-14 4.0.7 hotfix")
 
 
 
-# def get_version_number(file_path): 
-  
-#     File_information = GetFileVersionInfo(file_path, "\\") 
-  
-#     ms_file_version = File_information['FileVersionMS'] 
-#     ls_file_version = File_information['FileVersionLS'] 
-  
-#     return [str(HIWORD(ms_file_version)), str(LOWORD(ms_file_version)), 
-#             str(HIWORD(ls_file_version)), str(LOWORD(ls_file_version))] 
-  
-# file_path = r'Z:\Testy\2024-05-14 4.0.7 hotfix\MainFiles\V4_I10x64\DoorsWindows.dll'
-  
-# version = ".".join(get_version_number(file_path)) 
-  
-# print(version)
+
 
 # path = "Z:\\Testy\\2024-05-14 4.0.7 hotfix\\MainFiles\\V4_I10x64\\DoorsWindows.dll"
 
